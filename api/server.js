7ab05699f6bb4bc4555ecc49aa95becdefbd27dc
Tate Keller <tatek1993@gmail.com>
2020-04-07 18:32:16 -0500

    set up server and file structure

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const hubsRouter = require('../hubs/hubs-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    const query = req.query;
+
+    console.log('query', query);
+
+    res.status(200).json(query);
+});
+
+server.use('/api/posts', hubsRouter);
+
+module.exports = server;
